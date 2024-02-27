bd289eea0d9d7de27116fd86272d427c49606468
NotDiego7 <Lopez.d7@outlook.com>
2024-02-27 12:27:02 -0600

    Progress

--- my-react-app/src/TestApp.js
+++ my-react-app/src/TestApp.js
@@ -129,58 +129,102 @@ Part 2:
 
 
 
-import React from "react"
-import ReactDOM from "react-dom"
+// import React from "react"
+// import ReactDOM from "react-dom"
 
-/**
-Challenge: 
+// /**
+// Challenge: 
 
-- Move the `footer` into its own component called "Footer" 
-  and render that component inside the Page component.
-- Move the `h1` and `ol` together into another component
-  called "MainContent" and render inside Page as well.
-*/
+// - Move the `footer` into its own component called "Footer" 
+//   and render that component inside the Page component.
+// - Move the `h1` and `ol` together into another component
+//   called "MainContent" and render inside Page as well.
+// */
 
-function Header() {
+// function Header() {
+//     return (
+//         <header>
+//             <nav>
+//                 <img src="./react-logo.png" width="40px" alt="React Logo." />
+//             </nav>
+//         </header>
+//     )
+// }
+
+// function Footer() {
+//     return (
+//         <footer>
+//             <small>© 2021 Ziroll development. All rights reserved.</small>
+//         </footer>
+//     )
+// }
+
+// function Main() {
+//     return (
+//         <div>
+//             <h1>Reasons I'm excited to learn React</h1>
+//             <ol>
+//                 <li>It's a popular library, so I'll be 
+//                 able to fit in with the cool kids!</li>
+//                 <li>I'm more likely to get a job as a developer
+//                 if I know React</li>
+//             </ol>
+//         </div>
+//     )
+// }
+
+// function Page() {
+//     return (
+//         <div>
+//             <Header />
+//             <Main />
+//             <Footer />
+//         </div>
+//     )
+// }
+
+// ReactDOM.render(<Page />, document.getElementById("root"))
+
+/* -------------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+
+import React from "react";
+import "./TestApp.css"
+import logo from "./logo.svg";
+
+function AppHeader() {
     return (
         <header>
-            <nav>
-                <img src="./react-logo.png" width="40px" alt="React Logo." />
+            <nav className="nav-elem">
+                <img className="nav-logo" src={logo} alt="React Logo."></img>
+                <h1>ReactFacts</h1>
+                <button>Sign in</button>
             </nav>
         </header>
     )
 }
 
-function Footer() {
+function AppMainContent() {
     return (
-        <footer>
-            <small>© 2021 Ziroll development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-function Main() {
-    return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be 
-                able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer
-                if I know React</li>
-            </ol>
+        <div className="main-container">
+            <h1>Fun Facts about React</h1>
+            <ul>
+                <li>Released in 2013</li>
+                <li>Created by Jordan Walke</li>
+                <li>Mantained by Meta</li>
+                <li>Powers some of big-tech's products.</li>
+            </ul>
         </div>
     )
 }
 
-function Page() {
+function App() {
     return (
         <div>
-            <Header />
-            <Main />
-            <Footer />
+            <AppHeader />
+            <AppMainContent />
         </div>
     )
 }
 
-ReactDOM.render(<Page />, document.getElementById("root"))
+export default App
